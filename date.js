20ae208d87a8c7c65eeefef0b1ecc3ac4e669bd1
moriken <moruku36@yahoo.co.jp>
2018-02-02 23:21:03 +0900

    ko

---
+++ date.js
@@ -0,0 +1,8 @@
+var dat = new Date(2016,11,25,11,37,15,999);
+console.log(dat);
+console.log(dat.getFullYear());
+console.log(dat.getMonth());
+console.log(dat.getDate());
+console.log(dat.getDay());
+console.log(dat.getHours());
+console.log(dat.getMinutes());
