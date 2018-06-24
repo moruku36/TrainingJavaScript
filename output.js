4650744d966293539d4c8bf3ce2b25b234cc640b
Mori Kentaro <morikentaro@Moris-MacBook-Pro.local>
2018-06-24 11:18:12 +0900

    久しぶりのコミット

---
+++ output.js
@@ -0,0 +1,11 @@
+
+function output(){
+	var area = document.getElementById("outputArea");
+	area.innerHTML = "ボタンが押されました！";
+
+
+
+<body>
+	<input type="button" value="button" onclick="output()">
+	<DIV id = "outputArea"></div>
+</body>
---
+++ pointcard.js
@@ -0,0 +1,22 @@
+
+<html>
+<head> 
+
+<meta charset="utf-8">
+<title>List1-8 </title>
+
+</head>
+
+<body>
+	<script>
+	class PointCard{
+	constructor(name){
+	this.name = name;
+	this.point + 0;
+	console.log(
+	this.name + "のpointは、" + this.point + "ptです。");	
+	}
+}
+	</script>
+</body>
+</html>
